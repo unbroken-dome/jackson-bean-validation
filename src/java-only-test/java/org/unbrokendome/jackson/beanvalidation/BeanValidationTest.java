bf779834def0998e4a19c51d83e2226484472159
Till Krullmann <till.krullmann@gmail.com>
2021-05-08 14:57:52 +0200

    Upgrade Gradle to 7.0, use Maven Central repository instead of JCenter, drop bintray publishing

---
+++ src/java-only-test/java/org/unbrokendome/jackson/beanvalidation/BeanValidationTest.java
@@ -0,0 +1,50 @@
+package org.unbrokendome.jackson.beanvalidation;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import org.hibernate.validator.messageinterpolation.ParameterMessageInterpolator;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.unbrokendome.jackson.beanvalidation.BeanValidationModule;
+import org.unbrokendome.jackson.beanvalidation.JsonValidated;
+
+import javax.validation.ConstraintViolationException;
+import javax.validation.Validation;
+import javax.validation.ValidatorFactory;
+import javax.validation.constraints.NotNull;
+
+class BeanValidationTest {
+
+    @Test
+    void testBeanValidation() throws Exception {
+        String invalidJson = "{\"not_null\": null}";
+
+        ValidatorFactory validatorFactory = Validation.byDefaultProvider()
+                .configure()
+                .messageInterpolator(new ParameterMessageInterpolator())
+                .buildValidatorFactory();
+
+        ObjectMapper objectMapper = new ObjectMapper()
+                .registerModule(new BeanValidationModule(validatorFactory))
+                .setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE);
+
+        Assertions.assertThrows(ConstraintViolationException.class, () -> {
+            objectMapper.readValue(invalidJson, TestSerializable.class);
+        });
+    }
+
+    @JsonValidated
+    public static class TestSerializable {
+        @NotNull
+        private final String notNull;
+
+        @JsonCreator
+        public TestSerializable(
+                @NotNull @JsonProperty(value = "not_null", required = true) String notNull
+        ) {
+            this.notNull = notNull;
+        }
+    }
+}
